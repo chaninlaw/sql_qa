26f0a8b8e11657388f331aee33433a6306ca92b6
ChaninL <chaninlawlert@gmail.com>
2025-09-03 23:50:47 +0700

    Refactor build_llm function to include temperature parameter for model initialization

--- chat/llm/openai.py
+++ chat/llm/openai.py
@@ -15,6 +15,7 @@ if not os.environ.get("OPENAI_API_KEY"):
 type OpenAIModel = Literal["gpt-4o-mini"]
 
 
-def build_llm(model: OpenAIModel = "gpt-4o-mini"):
-    llm = init_chat_model(model, model_provider="openai")
+def build_llm(model: OpenAIModel = "gpt-4o-mini", temperature=0.2):
+    llm = init_chat_model(model, model_provider="openai",
+                          temperature=temperature)
     return llm
